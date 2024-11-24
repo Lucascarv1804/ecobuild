4c40b42568503b2738b61867d0f89b615554bdaa
Lucascarv1804 <lucascarv1804@gmail.com>
2024-11-24 15:08:33 -0300

    Responsividade

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -71,7 +71,11 @@ document.addEventListener("DOMContentLoaded", function () {
 // Header Mobile
 const toggleBtn = document.querySelector(".toggle-btn");
 const menuMobile = document.querySelector(".menu-mobile");
+const toggleIcon = document.querySelector(".bi-list");
 
 toggleBtn.addEventListener("click", () => {
     menuMobile.classList.toggle("off");
+
+    toggleIcon.classList.toggle("bi-list");
+    toggleIcon.classList.toggle("bi-x-lg");
 });
